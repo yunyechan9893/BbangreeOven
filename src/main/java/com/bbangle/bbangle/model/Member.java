6fc803e8f86f838e09a705f6a2e562d45be9c487
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-01-03 18:37:26 +0900

    feat: 사용하는 Entity DB와 맞추기
    column 타입과 현재 매칭되는 db에서의 테이블 모두 매칭, validate 옵션으로 확인

---
+++ src/main/java/com/bbangle/bbangle/model/Member.java
@@ -0,0 +1,42 @@
+package com.bbangle.bbangle.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Table(name = "member")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "birth")
+    private String birth;
+
+    @Column(name = "profile")
+    private String profile;
+
+    @Column(name = "is_deleted", columnDefinition = "tinyint")
+    private boolean isDeleted;
+
+}
