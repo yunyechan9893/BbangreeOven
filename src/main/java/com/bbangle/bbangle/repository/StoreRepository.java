adca0c5e9b68f0c9600d294e03812852d9e5e88d
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-01-07 02:03:08 +0900

    feat: store, product JpaRepository 작성

---
+++ src/main/java/com/bbangle/bbangle/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package com.bbangle.bbangle.repository;
+
+import com.bbangle.bbangle.model.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+}
