a334dc562b75050f6445129dbed338016849d2e7
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-01-07 02:01:54 +0900

    feat: List 반환을 위한 Response 타입 지정

---
+++ src/main/java/com/bbangle/bbangle/dto/BoardResponseDto.java
@@ -0,0 +1,17 @@
+package com.bbangle.bbangle.dto;
+
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record BoardResponseDto(
+        Long boardId,
+        Long storeId,
+        String storeName,
+        String thumbnail,
+        String title,
+        int price,
+        ProductTagDto tagDto
+) {
+
+}
