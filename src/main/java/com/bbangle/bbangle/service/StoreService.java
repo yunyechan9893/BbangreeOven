a062fbb1fa0f34aa0ac17efb9bb9cb44788eebbe
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-21 16:29:35 +0900

    feat: store 전체 조회 서비스 로직 추가

--- src/main/java/com/bbangle/bbangle/service/StoreService.java
+++ src/main/java/com/bbangle/bbangle/service/StoreService.java
@@ -1,8 +1,13 @@
 package com.bbangle.bbangle.service;
 
 import com.bbangle.bbangle.dto.StoreDetailResponseDto;
+import com.bbangle.bbangle.dto.StoreResponseDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface StoreService {
     StoreDetailResponseDto getStoreDetailResponse(Long StoreId);
+
+    Slice<StoreResponseDto> getList(Pageable pageable);
 
 }
