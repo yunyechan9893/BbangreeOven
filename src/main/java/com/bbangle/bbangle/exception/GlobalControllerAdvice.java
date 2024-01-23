e8096d65d1d02953a0e22da10bace0b0f1b28a03
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-01-23 22:08:16 +0900

    feat: member가 존재하지 않는 경우 예외처리

--- src/main/java/com/bbangle/bbangle/exception/GlobalControllerAdvice.java
+++ src/main/java/com/bbangle/bbangle/exception/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.bbangle.bbangle.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +16,11 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex){
         return ResponseEntity.badRequest().body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleMemberNotFoundException(MemberNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
     }
 
 }
