410751ca877b10fba47d7198836c416ceb837408
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-25 17:05:06 +0900

    feat: 게시판은 WishListFolder에 추가하는 API 추가 및 필요한 Request 추가

---
+++ src/main/java/com/bbangle/bbangle/controller/WishListProductController.java
@@ -0,0 +1,31 @@
+package com.bbangle.bbangle.controller;
+
+import com.bbangle.bbangle.dto.WishRequestDto;
+import com.bbangle.bbangle.service.WishListProductService;
+import com.bbangle.bbangle.util.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/boards/{boardId}/wish")
+@RequiredArgsConstructor
+public class WishListProductController {
+
+    private final WishListProductService wishListProductService;
+
+    @PatchMapping
+    public ResponseEntity<Void> wish(@PathVariable Long boardId,
+                                     @RequestBody WishRequestDto wishRequest){
+        Long memberId = SecurityUtils.getMemberId();
+        wishListProductService.wish(memberId, boardId, wishRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
