83db3c78e5746d39dd4aaa5651e6bb5d1abbaa24
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-25 17:02:49 +0900

    refactor: wishList 폴더 crud 에서 member 정보 가져오는 메서드 변경

--- src/main/java/com/bbangle/bbangle/controller/WishListFolderController.java
+++ src/main/java/com/bbangle/bbangle/controller/WishListFolderController.java
@@ -5,11 +5,11 @@ import com.bbangle.bbangle.dto.FolderRequestDto;
 import com.bbangle.bbangle.dto.FolderResponseDto;
 import com.bbangle.bbangle.dto.FolderUpdateDto;
 import com.bbangle.bbangle.service.WishListFolderService;
+import com.bbangle.bbangle.util.SecurityUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,34 +27,31 @@ public class WishListFolderController {
     private final WishListFolderService folderService;
 
     @PostMapping
-    public ResponseEntity<Void> make(@RequestBody @Valid FolderRequestDto requestDto,
-                                  Authentication authentication){
-        Long memberId = 1L;
+    public ResponseEntity<Void> make(@RequestBody @Valid FolderRequestDto requestDto){
+        Long memberId = SecurityUtils.getMemberId();
         folderService.create(memberId, requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping
-    public ResponseEntity<List<FolderResponseDto>> getList(Authentication authentication){
-        Long memberId = 1L;
+    public ResponseEntity<List<FolderResponseDto>> getList(){
+        Long memberId = SecurityUtils.getMemberId();
         return ResponseEntity.ok(folderService.getList(memberId));
     }
 
     @PatchMapping("/{folderId}")
     public ResponseEntity<FolderResponseDto> update(@PathVariable Long folderId,
-                                                    @Valid FolderUpdateDto updateDto,
-                                                    Authentication authentication){
-        Long memberId = 1L;
+                                                    @RequestBody @Valid FolderUpdateDto updateDto){
+        Long memberId = SecurityUtils.getMemberId();
         folderService.update(memberId, folderId, updateDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/{folderId}")
-    public ResponseEntity<Void> delete(@PathVariable Long folderId,
-                                     Authentication authentication){
-        Long memberId = 1L;
+    public ResponseEntity<Void> delete(@PathVariable Long folderId){
+        Long memberId = SecurityUtils.getMemberId();
         folderService.delete(folderId, memberId);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
