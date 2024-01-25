8e203a10fba2d3a16dce6f92a462a754302c3ea6
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-25 17:07:48 +0900

    feat: 내가 위시리스트 폴더에서 추가한 게시글 작성 로직 추가

--- src/main/java/com/bbangle/bbangle/service/impl/BoardServiceImpl.java
+++ src/main/java/com/bbangle/bbangle/service/impl/BoardServiceImpl.java
@@ -1,8 +1,16 @@
 package com.bbangle.bbangle.service.impl;
 
+import java.util.List;
 import com.bbangle.bbangle.dto.BoardDetailResponseDto;
 import com.bbangle.bbangle.dto.BoardResponseDto;
+import com.bbangle.bbangle.exception.MemberNotFoundException;
+import com.bbangle.bbangle.model.Member;
+import com.bbangle.bbangle.model.WishlistFolder;
+import com.bbangle.bbangle.model.WishlistProduct;
 import com.bbangle.bbangle.repository.BoardRepository;
+import com.bbangle.bbangle.repository.MemberRepository;
+import com.bbangle.bbangle.repository.WishListFolderRepository;
+import com.bbangle.bbangle.repository.WishListProductRepository;
 import com.bbangle.bbangle.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +23,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
+    private final WishListFolderRepository folderRepository;
+    private final WishListProductRepository wishListProductRepository;
 
     @Override
     @Transactional(readOnly = true)
@@ -40,6 +51,23 @@ public class BoardServiceImpl implements BoardService {
     @Transactional(readOnly = true)
     public BoardDetailResponseDto getBoardDetailResponse(Long boardId) {
         return boardRepository.getBoardDetailResponseDto(boardId);
+    }
+
+    public Slice<BoardResponseDto> getPostInFolder(Long memberId, String sort, Long folderId, Pageable pageable) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new);
+        WishlistFolder folder = folderRepository.findByMemberAndId(member, folderId)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 폴더입니다."));
+        if(folder.isDeleted()){
+            throw new IllegalArgumentException("존재하지 않는 폴더입니다.");
+        }
+
+        List<Long> boardIds = wishListProductRepository.findAllByWishlistFolder(folder)
+            .stream()
+            .map(product -> product.getBoard().getId())
+            .toList();
+
+        return boardRepository.getAllByFolder(sort, pageable, folderId, boardIds);
     }
 
 }
