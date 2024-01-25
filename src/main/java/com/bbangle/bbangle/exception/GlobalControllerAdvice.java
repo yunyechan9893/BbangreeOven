6ea3985cd86ba4be448362154b31426316681529
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-25 17:02:11 +0900

    feat: validation 시 나오는 method 에러 처리 추가

--- src/main/java/com/bbangle/bbangle/exception/GlobalControllerAdvice.java
+++ src/main/java/com/bbangle/bbangle/exception/GlobalControllerAdvice.java
@@ -1,12 +1,19 @@
 package com.bbangle.bbangle.exception;
 
+import java.util.List;
+import java.util.Objects;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
+
+    public static final String DEFAULT_MESSAGE = "유효하지 않은 요청 입니다.";
 
     @ExceptionHandler(CategoryTypeException.class)
     public ResponseEntity<ErrorResponse> handleCategoryTpeException(CategoryTypeException ex){
@@ -21,6 +28,21 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleMemberNotFoundException(MemberNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> errors = e.getFieldErrors();
+        String message = errors.stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .filter(Objects::nonNull)
+            .findFirst()
+            .orElse(DEFAULT_MESSAGE);
+
+        ErrorResponse errorResponse = new ErrorResponse(message);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(errorResponse);
     }
 
 }
