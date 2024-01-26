bc8241147332de321c723659e1d51d0577bbe785
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-01-26 17:44:09 +0900

    Feat: 자동완성 기능 테스트 구현

--- src/test/java/com/bbangle/bbangle/service/SearchServiceTest.java
+++ src/test/java/com/bbangle/bbangle/service/SearchServiceTest.java
@@ -4,6 +4,7 @@ import com.bbangle.bbangle.model.RedisEnum;
 import com.bbangle.bbangle.repository.InitRepository;
 import com.bbangle.bbangle.repository.RedisRepository;
 import com.bbangle.bbangle.util.KomoranUtil;
+import com.bbangle.bbangle.util.TrieUtil;
 import kr.co.shineware.nlp.komoran.model.KomoranResult;
 import kr.co.shineware.nlp.komoran.model.Token;
 import org.junit.jupiter.api.Assertions;
@@ -25,6 +26,37 @@ public class SearchServiceTest {
 
     @Autowired
     RedisRepository redisRepository;
+
+    @Test
+    public void loadData(){
+        searchService.loadData();
+    }
+
+    @Test
+    public void trieUtilTest(){
+        TrieUtil trieUtil = new TrieUtil();
+
+        trieUtil.insert("비건 베이커리");
+        trieUtil.insert("비건");
+        trieUtil.insert("비건 베이커리 짱짱");
+        trieUtil.insert("초코송이");
+
+        var resultOne = trieUtil.autoComplete("초", 1);
+        Assertions.assertEquals(resultOne,List.of("초코송이"));
+        Assertions.assertEquals(resultOne.size(),1);
+
+        var resultTwo = trieUtil.autoComplete("비", 2);
+        Assertions.assertEquals(resultTwo,List.of("비건", "비건 베이커리"));
+        Assertions.assertEquals(resultTwo.size(),2);
+
+        var resultThree = trieUtil.autoComplete("비", 3);
+        Assertions.assertEquals(resultThree,List.of("비건", "비건 베이커리", "비건 베이커리 짱짱"));
+        Assertions.assertEquals(resultThree.size(),3);
+
+        var resultFour = trieUtil.autoComplete("바", 3);
+        Assertions.assertEquals(resultFour,List.of());
+        Assertions.assertEquals(resultFour.size(),0);
+    }
 
     @Test
     public void getAllBoardTitleTest(){
