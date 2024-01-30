a6e22c397aba815ac8bdb4a502203027f337ee91
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-01-30 17:40:55 +0900

    Refactor: 회원 비회원 판별 기능 추가

--- src/main/java/com/bbangle/bbangle/controller/SearchController.java
+++ src/main/java/com/bbangle/bbangle/controller/SearchController.java
@@ -1,8 +1,10 @@
 package com.bbangle.bbangle.controller;
 
 import com.bbangle.bbangle.dto.KeywordDto;
+import com.bbangle.bbangle.dto.MessageResDto;
 import com.bbangle.bbangle.dto.SearchResponseDto;
 import com.bbangle.bbangle.service.SearchService;
+import com.bbangle.bbangle.util.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,26 +14,45 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/search")
 public class SearchController {
+    private final Long ANONYMOUS_ID = 1L;
 
     @Autowired
     SearchService searchService;
 
     @GetMapping
     public ResponseEntity<SearchResponseDto> getSearchedBoard(
-            @RequestParam
+            @RequestParam(value = "keyword")
             String keyword
     ){
         return ResponseEntity.ok().body(searchService.getSearchResult(keyword));
+    }
+
+    @PostMapping
+    public ResponseEntity<MessageResDto> saveKeyword(
+            @RequestParam
+            String keyword
+    ){
+        Long memberId = SecurityUtils.getUserIdWithAnonymous();
+
+        // 유저가 아니라면 비회원 아이디를 사용
+        if (memberId == null){
+            memberId = ANONYMOUS_ID;
+        }
+
+        try {
+            searchService.saveKeyword(memberId, keyword);
+            return ResponseEntity.ok().body(new MessageResDto("검색어 저장 완료"));
+        } catch (Exception e){
+            return ResponseEntity.ok().body(new MessageResDto("검색어 저장 실패"));
+        }
     }
 
     @GetMapping("/recency")
     public ResponseEntity<List<KeywordDto>> getRecencyKeyword(){
 
         // 임의로 구성
-        Long memberId = 1L;
-
+        Long memberId = SecurityUtils.getMemberId();
         List<KeywordDto> keywords = searchService.getRecencyKeyword(memberId);
-
         var successResponse = ResponseEntity.ok().body(keywords);
 
         return successResponse;
