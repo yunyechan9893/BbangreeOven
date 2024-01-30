765db40f70cceb9645577a916933e693a1c04662
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-30 19:45:11 +0900

    feat: 사용하지 않는 WishListProduct 관련 repository 삭제
    한방쿼리로 board에서 수행하여 더이상 필요 없는 repository 삭제

--- src/main/java/com/bbangle/bbangle/repository/WishListProductRepository.java
+++ src/main/java/com/bbangle/bbangle/repository/WishListProductRepository.java
@@ -3,13 +3,11 @@ package com.bbangle.bbangle.repository;
 import java.util.Optional;
 import com.bbangle.bbangle.model.WishlistFolder;
 import com.bbangle.bbangle.model.WishlistProduct;
-import com.bbangle.bbangle.repository.queryDsl.WishListProductQueryDSLRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface WishListProductRepository extends JpaRepository<WishlistProduct, Long>,
-    WishListProductQueryDSLRepository {
+public interface WishListProductRepository extends JpaRepository<WishlistProduct, Long>{
 
     @Query(value = "select wish from WishlistProduct wish where wish.board.id = :boardId and wish.wishlistFolder = :folder")
     Optional<WishlistProduct> findByBoardAndFolderId(@Param("boardId") Long boardId, @Param("folder")WishlistFolder folder);
