619f628393ce844b35e0fdbd0a25452921c2ca39
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-02-06 17:15:22 +0900

    refactor: 시간 정보도 redis에 같이 저장하기 위한 직렬화 어노테이션 추가

--- src/main/java/com/bbangle/bbangle/config/ranking/BoardLikeInfo.java
+++ src/main/java/com/bbangle/bbangle/config/ranking/BoardLikeInfo.java
@@ -1,12 +1,20 @@
 package com.bbangle.bbangle.config.ranking;
 
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-@AllArgsConstructor
-public class BoardLikeInfo {
-
-    private final int score;
-    private final LocalDateTime createdAt;
+public record BoardLikeInfo(
+    Long boardId,
+    double score,
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    LocalDateTime createdAt,
+    ScoreType scoreType
+) {
 
 }
