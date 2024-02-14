1aa114f3737e3cb065b814544b56cf382f51a068
sikdong <zmstnv1119@naver.com>
2024-02-14 22:35:36 +0900

    style : 랜딩페이지 permitAll() 경로 변경

--- src/main/java/com/bbangle/bbangle/config/WebOAuthSecurityConfig.java
+++ src/main/java/com/bbangle/bbangle/config/WebOAuthSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebOAuthSecurityConfig {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/token").permitAll()
                         .requestMatchers("/api/v1/search/**").permitAll()
-                        .requestMatchers("/api/v1/landingPage").permitAll()
+                        .requestMatchers("/api/v1/landingpage").permitAll()
                         .requestMatchers("/api/v1/store/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/boards/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/notice/**").permitAll()
