98f94a32cedb0a33a1a7cff2e74fda42af3d88a0
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-02-20 16:31:48 +0900

    Style: 불필요한 코드 정리

--- src/main/java/com/bbangle/bbangle/config/RedisConfig.java
+++ src/main/java/com/bbangle/bbangle/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.bbangle.bbangle.config;
 
 import com.bbangle.bbangle.config.ranking.BoardLikeInfo;
-import com.fasterxml.jackson.databind.ser.std.NumberSerializers.LongSerializer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
@@ -53,41 +51,4 @@ public class RedisConfig {
 
         return redisTemplate;
     }
-
-//    private final RedisProperties redisProperties;
-//
-//    // RedisProperties로 yaml에 저장한 host, post를 연결
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory() {
-//        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
-//    }
-//
-//    // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//
-//        // 값 직렬화 방식으로 StringRedisSerializer 사용
-//        redisTemplate.setValueSerializer(new StringRedisSerializer());
-//
-//        redisTemplate.setConnectionFactory(redisConnectionFactory());
-//        return redisTemplate;
-//    }
-//
-////    @Bean
-////    public RedisTemplate<String, Object> boardLikeInfoRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-////        RedisTemplate<String, Object> template = new RedisTemplate<>();
-////        template.setConnectionFactory(redisConnectionFactory);
-////
-////        // Key Serializer
-////        template.setKeySerializer(new StringRedisSerializer());
-////
-////        // BoardLikeInfo 전용 Value Serializer 설정
-////        Jackson2JsonRedisSerializer<BoardLikeInfo> serializer = new Jackson2JsonRedisSerializer<>(BoardLikeInfo.class);
-////        template.setValueSerializer(serializer);
-////
-////        return template;
-////    }
-
 }
