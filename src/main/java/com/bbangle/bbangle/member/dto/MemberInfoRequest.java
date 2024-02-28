5e52e9003d16e6d5e48fb2fe73b64e147cda5ee0
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-02-28 23:59:16 +0900

    feat: 핸드폰 정보 관련 내용 추가

--- src/main/java/com/bbangle/bbangle/member/dto/MemberInfoRequest.java
+++ src/main/java/com/bbangle/bbangle/member/dto/MemberInfoRequest.java
@@ -7,7 +7,9 @@ public record MemberInfoRequest(
     @Size(max = 20, message = "nickname 은 20자 이하만 등록 가능합니다.")
     @NotNull(message = "닉네임은 필수입니다.")
     String nickname,
-    String phone,
+    @Size(min = 11, max = 11, message = "전화번호를 다시 확인해주세요")
+    @NotNull(message = "전화번호는 필수입니다.")
+    String phoneNumber,
     String birthDate,
     boolean isAllowingMarketing,
     @NotNull(message = "개인정보 약관 동의는 필수입니다.")
