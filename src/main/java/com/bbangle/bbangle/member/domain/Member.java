c0890aede5c4ae14fab1a6fe5390a05dd9dc53e1
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-02-28 23:58:28 +0900

    feat: member, 동의 항목 entity 수정, update info 메서드 추가

--- src/main/java/com/bbangle/bbangle/member/domain/Member.java
+++ src/main/java/com/bbangle/bbangle/member/domain/Member.java
@@ -1,6 +1,7 @@
 package com.bbangle.bbangle.member.domain;
 
 import com.bbangle.bbangle.member.dto.MemberInfoRequest;
+import com.bbangle.bbangle.member.exception.UserValidator;
 import com.bbangle.bbangle.model.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -44,10 +45,12 @@ public class Member extends BaseEntity implements UserDetails {
 
     @Column(name = "is_deleted", columnDefinition = "tinyint")
     private boolean isDeleted;
-  
+
     @Builder
-    public Member(Long id, String email, String phone, String name, String nickname,
-                  String birth, boolean isDeleted, String profile)  {
+    public Member(
+        Long id, String email, String phone, String name, String nickname,
+        String birth, boolean isDeleted, String profile
+    ) {
         this.id = id;
         this.email = email;
         this.phone = phone;
@@ -93,15 +96,25 @@ public class Member extends BaseEntity implements UserDetails {
         return true;
     }
 
-    public Member update(String nickname){
+    public Member update(String nickname) {
         this.nickname = nickname;
         return this;
     }
 
     public void updateInfo(MemberInfoRequest request) {
+        if (request.birthDate() != null) {
+            UserValidator.validateBirthDate(request.birthDate());
+        }
+        UserValidator.validatePhoneNumber(request.phoneNumber());
+        UserValidator.validateNickname(request.nickname());
+
         this.birth = request.birthDate();
         this.nickname = request.nickname();
-        this.phone = request.phone();
+        this.phone = request.phoneNumber();
+    }
+
+    public void updateProfile(String imgUrl) {
+        this.profile = imgUrl;
     }
 
 }
--- src/main/java/com/bbangle/bbangle/member/domain/SignatureAgreement.java
+++ src/main/java/com/bbangle/bbangle/member/domain/SignatureAgreement.java
@@ -3,6 +3,8 @@ package com.bbangle.bbangle.member.domain;
 import com.bbangle.bbangle.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,8 +33,12 @@ public class SignatureAgreement {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @Column(name = "name")
-    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", columnDefinition = "varchar(100)")
+    private Agreement name;
+
+    @Column(name = "date_of_signature")
+    private LocalDateTime dateOfSignature;
 
     @Column(name = "agreement_status", columnDefinition = "tinyint")
     private boolean agreementStatus;
