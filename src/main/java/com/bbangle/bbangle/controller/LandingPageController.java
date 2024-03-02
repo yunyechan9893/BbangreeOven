d8daf5b2ef874fba31b8d1462d08a4f8de054681
sikdong <zmstnv1119@naver.com>
2024-03-03 00:04:00 +0900

    refactor : 랜딩페이지 반환 타입 수정

--- src/main/java/com/bbangle/bbangle/controller/LandingPageController.java
+++ src/main/java/com/bbangle/bbangle/controller/LandingPageController.java
@@ -1,5 +1,6 @@
 package com.bbangle.bbangle.controller;
 
+import com.bbangle.bbangle.dto.MessageResDto;
 import com.bbangle.bbangle.dto.RequestEmailDto;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +16,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @Slf4j
@@ -25,7 +24,7 @@ public class LandingPageController {
     private static final String DIRECTORY_PATH = "/etc/bbangle";
     static final String FILE_NAME = "landingPageUserEmail.txt";
     @PostMapping("/landingpage")
-    public ResponseEntity<Map<String, String>> getUserEmail(@Valid @RequestBody RequestEmailDto requestEmailDto){
+    public ResponseEntity<MessageResDto> getUserEmail(@Valid @RequestBody RequestEmailDto requestEmailDto){
         String email = requestEmailDto.getEmail()+",";
 
         //for local
@@ -33,7 +32,6 @@ public class LandingPageController {
         //for server
         // 파일 오브젝트 생성하기
         Path filePath = Paths.get(DIRECTORY_PATH, FILE_NAME);
-        Map<String, String> messageMap = new HashMap<>();
         try {
             //만약 경로가 없으면 생성하기
             Files.createDirectories(filePath.getParent());
@@ -41,18 +39,15 @@ public class LandingPageController {
             // 파일이 있는지 확인하고 없으면 생성
             if (Files.notExists(filePath)) {
                 Files.createFile(filePath);
-                log.debug("File created: " + filePath.toString());
             }
 
             // 파일에 이메일 추가하기
             Files.write(filePath, email.getBytes(), StandardOpenOption.APPEND);
 
-            messageMap.put("message", "success");
-            return ResponseEntity.ok(messageMap);
+            return ResponseEntity.ok(new MessageResDto("success"));
         } catch (IOException e) {
             log.error("getUserEmail() >>>> file io {}", e.getMessage());
-            messageMap.put("message", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageMap);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResDto(e.getMessage()));
         }
     }
 }
