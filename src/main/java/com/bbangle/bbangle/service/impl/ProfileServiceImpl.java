38416a40f269b107802cfa675339987f06be3dda
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-02 16:15:19 +0900

    feat: info update 서비스 로직 추가

--- src/main/java/com/bbangle/bbangle/service/impl/ProfileServiceImpl.java
+++ src/main/java/com/bbangle/bbangle/service/impl/ProfileServiceImpl.java
@@ -5,12 +5,14 @@ import com.bbangle.bbangle.dto.ProfileInfoResponseDto;
 import com.bbangle.bbangle.exception.DuplicateNicknameException;
 import com.bbangle.bbangle.exception.MemberNotFoundException;
 import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.member.dto.InfoUpdateRequest;
 import com.bbangle.bbangle.repository.ProfileRepository;
 import com.bbangle.bbangle.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,8 +32,11 @@ public class ProfileServiceImpl implements ProfileService {
                 .build();
     }
 
-    public MessageResDto updateProfileInfo(ProfileInfoResponseDto profileInfoResponseDto, Long memberId) {
-        return null;
+    @Transactional
+    public void updateProfileInfo(InfoUpdateRequest request, Long memberId) {
+        Member member = profileRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new);
+        member.update(request);
     }
 
     @Override
@@ -41,4 +46,5 @@ public class ProfileServiceImpl implements ProfileService {
             throw new DuplicateNicknameException();
         }
     }
+
 }
