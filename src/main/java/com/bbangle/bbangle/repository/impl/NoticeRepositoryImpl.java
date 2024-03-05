67c9ef487567ec0ef66dc9e6144874e67747d58d
sikdong <zmstnv1119@naver.com>
2024-03-05 22:41:47 +0900

    refactor : 공지사항 조회 시 전체 데이터 Count query 최적화

--- src/main/java/com/bbangle/bbangle/repository/impl/NoticeRepositoryImpl.java
+++ src/main/java/com/bbangle/bbangle/repository/impl/NoticeRepositoryImpl.java
@@ -8,11 +8,13 @@ import com.bbangle.bbangle.repository.queryDsl.NoticeQueryDSLRepository;
 import com.querydsl.core.types.ConstantImpl;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.StringTemplate;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -44,15 +46,16 @@ public class NoticeRepositoryImpl implements NoticeQueryDSLRepository {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        return new PageImpl<>(notices, pageable, queryFactory
+        JPAQuery<NoticeResponseDto> totalQuery = queryFactory
                 .select(new QNoticeResponseDto(
                         notice.id,
                         notice.title,
                         FORMATTED_DATE
                 ))
                 .from(notice)
-                .orderBy(notice.createdAt.desc())
-                .fetch().size());
+                .orderBy(notice.createdAt.desc());
+
+        return PageableExecutionUtils.getPage(notices, pageable, totalQuery::fetchCount);
     }
 
     @Override
