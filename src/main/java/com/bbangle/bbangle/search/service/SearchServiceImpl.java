9b0d5c7057fef734b07537ee3d0244f3dcbf9a92
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-15 21:17:03 +0900

    refactor: import 추가

--- src/main/java/com/bbangle/bbangle/search/service/SearchServiceImpl.java
+++ src/main/java/com/bbangle/bbangle/search/service/SearchServiceImpl.java
@@ -23,6 +23,7 @@ import kr.co.shineware.nlp.komoran.model.KomoranResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
