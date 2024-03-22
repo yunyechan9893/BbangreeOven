686ed1d6e779144a50df9c6bba01cc40600bb588
sikdong <zmstnv1119@naver.com>
2024-03-22 22:59:55 +0900

    feat : OAuth 로그인 인가 코드 요청 위한 클래스 생성

---
+++ src/main/java/com/bbangle/bbangle/config/oauth/domain/OauthServerType.java
@@ -0,0 +1,13 @@
+package com.bbangle.bbangle.config.oauth.domain;
+
+import java.util.Locale;
+
+import static java.util.Locale.ENGLISH;
+
+public enum OauthServerType {
+    KAKAO;
+
+    public static OauthServerType fromName(String type){
+        return OauthServerType.valueOf(type.toUpperCase(ENGLISH));
+    }
+}
