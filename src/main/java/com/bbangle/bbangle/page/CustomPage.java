59bc1a86b5f8ed22fdb35e5e06015fa15ef3b96a
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-25 21:53:13 +0900

    feat: 게시판, 스토어 개수 추가

--- src/main/java/com/bbangle/bbangle/page/CustomPage.java
+++ src/main/java/com/bbangle/bbangle/page/CustomPage.java
@@ -1,9 +1,17 @@
 package com.bbangle.bbangle.page;
 
+import lombok.Builder;
+import net.minidev.json.annotate.JsonIgnore;
+
+@Builder
 public record CustomPage<T>(
     T content,
     int page,
-    boolean isLast
+    boolean isLast,
+    @JsonIgnore
+    int boardCnt,
+    @JsonIgnore
+    int storeCnt
 ) {
 
 }
