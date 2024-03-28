672e6243d1fa14c2c8d623aa2c7af80951fa8344
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-29 02:46:23 +0900

    feat: cursor 기반 페이지네이션에 필요한 클래스 추가

--- src/main/java/com/bbangle/bbangle/page/CustomPage.java
+++ src/main/java/com/bbangle/bbangle/page/CustomPage.java
@@ -1,17 +1,42 @@
 package com.bbangle.bbangle.page;
 
-import lombok.Builder;
+import com.bbangle.bbangle.board.dto.BoardResponseDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import net.minidev.json.annotate.JsonIgnore;
 
-@Builder
-public record CustomPage<T>(
-    T content,
-    int page,
-    boolean isLast,
-    @JsonIgnore
-    int boardCnt,
-    @JsonIgnore
-    int storeCnt
-) {
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public final class CustomPage<T> {
+
+    private final T content;
+    private final Long requestCursor;
+    private final Boolean hasNext;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long boardCnt;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long storeCnt;
+
+    public static CustomPage<List<BoardResponseDto>> from(
+        List<BoardResponseDto> boardList,
+        Long requestCursor,
+        boolean hasNext
+    ) {
+        return new CustomPage<>(boardList, requestCursor, hasNext);
+    }
+
+    public static CustomPage<List<BoardResponseDto>> from(
+        List<BoardResponseDto> boardList,
+        Long requestCursor,
+        boolean hasNext,
+        long boardCnt,
+        long storeCnt
+    ) {
+        return new CustomPage<>(boardList, requestCursor, hasNext, boardCnt, storeCnt);
+    }
 
 }
