da52b1a8275907a5b221cd3a697f26a466c9b906
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-30 00:07:51 +0900

    refactor: 기존 map으로 restTemplate 요청이 불가하여 MultiValueMap 사용

--- src/main/java/com/bbangle/bbangle/token/oauth/infra/kakao/client/KakaoApiClient.java
+++ src/main/java/com/bbangle/bbangle/token/oauth/infra/kakao/client/KakaoApiClient.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -19,21 +21,24 @@ import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VAL
 @RequiredArgsConstructor
 @Component
 public class KakaoApiClient {
-    private final KakaoOauthConfig kakaoOauthConfig;
-
     private static final String AUTHCODE_URL = "https://kauth.kakao.com/oauth/token";
     private static final String USERINFO_URL = "https://kapi.kakao.com/v2/user/me";
     private final RestTemplate restTemplate = new RestTemplate();
 
+    private final KakaoOauthConfig kakaoOauthConfig;
+
+
     public KakaoToken fetchToken(String authCode){
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", APPLICATION_FORM_URLENCODED_VALUE);
-        Map<String, String> body = new HashMap<>();
-        body.put("grant_type", "authorization_code");
-        body.put("client_id", kakaoOauthConfig.clientId());
-        body.put("redirect_uri", kakaoOauthConfig.redirectUri());
-        body.put("code", authCode);
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+        headers.add("Content-Type", APPLICATION_FORM_URLENCODED_VALUE);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", kakaoOauthConfig.clientId());
+        body.add("redirect_uri", kakaoOauthConfig.redirectUri());
+        body.add("code", authCode);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
         return restTemplate.postForObject(AUTHCODE_URL, request, KakaoToken.class);
     }
 
@@ -44,4 +49,5 @@ public class KakaoApiClient {
         ResponseEntity<KakaoMemberResponse> response = restTemplate.exchange(USERINFO_URL, HttpMethod.GET, request, KakaoMemberResponse.class);
         return response.getBody();
     }
+
 }
