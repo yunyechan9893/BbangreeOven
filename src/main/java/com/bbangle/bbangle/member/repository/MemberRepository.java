69030bc31cf9647430f03e89a81e2561234362b7
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-30 00:07:07 +0900

    feat: provider와 providerId로 member 찾는 로직 추가

--- src/main/java/com/bbangle/bbangle/member/repository/MemberRepository.java
+++ src/main/java/com/bbangle/bbangle/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.bbangle.bbangle.member.repository;
 
 import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.token.oauth.domain.OauthServerType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,5 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByProviderAndProviderId(OauthServerType provider, String providerId);
 
 }
