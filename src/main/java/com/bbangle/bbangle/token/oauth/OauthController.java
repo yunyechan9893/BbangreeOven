2dd14626b6614ad4d3528b0f32079390e9d1b596
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-30 22:20:19 +0900

    refactor: 사용하지 않는 oauth 삭제 후 token 만 받는 로직 도입

--- src/main/java/com/bbangle/bbangle/token/oauth/OauthController.java
+++ src/main/java/com/bbangle/bbangle/token/oauth/OauthController.java
@@ -2,11 +2,13 @@ package com.bbangle.bbangle.token.oauth;
 
 import com.bbangle.bbangle.token.oauth.domain.OauthServerType;
 import com.bbangle.bbangle.token.oauth.infra.kakao.dto.LoginTokenResponse;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/oauth")
@@ -17,10 +19,12 @@ public class OauthController {
 
     @GetMapping("/login/{oauthServerType}")
     ResponseEntity<LoginTokenResponse> login(
-            @PathVariable OauthServerType oauthServerType,
-            @RequestParam("code") String code
-    ){
-        LoginTokenResponse loginTokenResponse = oauthService.login(oauthServerType, code);
+        @PathVariable
+        OauthServerType oauthServerType,
+        @RequestParam("token")
+        String token
+    ) {
+        LoginTokenResponse loginTokenResponse = oauthService.login(oauthServerType, token);
         return ResponseEntity.ok(loginTokenResponse);
     }
 
--- src/main/java/com/bbangle/bbangle/token/oauth/OauthServerTypeConverter.java
+++ src/main/java/com/bbangle/bbangle/token/oauth/OauthServerTypeConverter.java
@@ -4,8 +4,10 @@ import com.bbangle.bbangle.token.oauth.domain.OauthServerType;
 import org.springframework.core.convert.converter.Converter;
 
 public class OauthServerTypeConverter implements Converter<String, OauthServerType> {
+
     @Override
     public OauthServerType convert(String source) {
         return OauthServerType.fromName(source);
     }
+
 }
