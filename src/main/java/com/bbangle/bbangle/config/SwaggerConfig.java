8df13fba718ee6c340a4fbbee01242ad516953f9
DongHyeon <ydh9517@naver.com>
2024-04-06 22:09:09 +0900

    코드리뷰 반영

--- src/main/java/com/bbangle/bbangle/config/SwaggerConfig.java
+++ src/main/java/com/bbangle/bbangle/config/SwaggerConfig.java
@@ -3,7 +3,7 @@ package com.bbangle.bbangle.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,15 +22,29 @@ public class SwaggerConfig {
                 .description("본 문서는 외부에 유출하지 마세요.\n실제 서비스시에는 profile를 변경해 비활성시킵니다.")
                 .version("1.0.0");
 
-        Components securityComponent = new Components()
-                .addSecuritySchemes("bearer ", new SecurityScheme()
+        OAuthFlows oAuthFlows = new OAuthFlows()
+                .authorizationCode(new OAuthFlow()
+                        .authorizationUrl("/oauth/authorize")
+                        .tokenUrl("/oauth/token")
+                        .refreshUrl("/oauth/refresh")
+                        .scopes(new Scopes().addString("read", "읽기권한 부여")));
+
+        SecurityScheme jwtSchemes = new SecurityScheme()
                         .type(SecurityScheme.Type.HTTP)
                         .scheme("bearer")
-                        .bearerFormat("JWT"));
+                        .bearerFormat("JWT");
+        SecurityScheme oauthSchemes = new SecurityScheme()
+                .type(SecurityScheme.Type.OAUTH2)
+                .flows(oAuthFlows);
 
         return new OpenAPI()
                 .info(info)
-                .components(securityComponent);
+                .addSecurityItem(new SecurityRequirement().addList("카카오 토큰 로그인"))
+                .addSecurityItem(new SecurityRequirement().addList("토큰 받아오기"))
+                .components(new Components()
+                        .addSecuritySchemes("카카오 토큰 로그인", jwtSchemes)
+                        .addSecuritySchemes("토큰 받아오기", oauthSchemes)
+                );
     }
 
     @Bean
