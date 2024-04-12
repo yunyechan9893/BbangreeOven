96f7f4330cc36fd307f60379582a4c59c5152446
shoeone96 <dlwnddnjs96@gmail.com>
2024-04-12 14:57:08 +0900

    Ranking Entity 추가

---
+++ src/main/java/com/bbangle/bbangle/ranking/domain/Ranking.java
@@ -0,0 +1,38 @@
+package com.bbangle.bbangle.ranking.domain;
+
+import com.bbangle.bbangle.board.domain.Board;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ranking")
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ranking {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_board_id")
+    private Board board;
+
+    @Column(name = "popular_score")
+    private Long popularScore;
+
+    @Column(name = "recommend_score")
+    private Long RecommendScore;
+
+}
