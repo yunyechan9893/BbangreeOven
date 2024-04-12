50ce0457f2777873457c1e997417c6448ab790a0
shoeone96 <dlwnddnjs96@gmail.com>
2024-04-12 10:13:55 +0900

    redis 랭킹 조회 시 최적화를 위한 id 전체 조회 쿼리

--- src/main/java/com/bbangle/bbangle/board/repository/BoardRepository.java
+++ src/main/java/com/bbangle/bbangle/board/repository/BoardRepository.java
@@ -1,12 +1,16 @@
 package com.bbangle.bbangle.board.repository;
 
 import com.bbangle.bbangle.board.domain.Board;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardQueryDSLRepository {
+
+    @Query(value = "select b.id from Board b")
+    List<Long> findAllIds();
 
 //    @Modifying(clearAutomatically = true)
 //    @Query("UPDATE Board b SET b.detail = :detailUrl WHERE b.id = :boardId ")
