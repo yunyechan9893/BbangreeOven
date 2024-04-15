1341d5935ef638b3bcd0df1fb72fd445532c6350
shoeone96 <dlwnddnjs96@gmail.com>
2024-04-15 14:48:55 +0900

    redis 기반 -> mariaDB ranking table 기반 초기데이터 검증 과정 적용

--- src/main/java/com/bbangle/bbangle/config/ranking/BoardWishListConfig.java
+++ src/main/java/com/bbangle/bbangle/config/ranking/BoardWishListConfig.java
@@ -1,38 +1,21 @@
 package com.bbangle.bbangle.config.ranking;
 
-import com.bbangle.bbangle.board.repository.BoardRepository;
-import com.bbangle.bbangle.util.RedisKeyUtil;
+import com.bbangle.bbangle.ranking.service.RankingService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.util.StopWatch;
 
 @Configuration
 @Slf4j
 @RequiredArgsConstructor
 public class BoardWishListConfig {
 
-    private final BoardRepository boardRepository;
-
-    @Qualifier("defaultRedisTemplate")
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RankingService rankingService;
 
     @PostConstruct
     public void init() {
-        StopWatch stopWatch = new StopWatch("redis ranking setting");
-        stopWatch.start();
-        boardRepository.findAllIds()
-            .forEach(boardId -> {
-                redisTemplate.opsForZSet()
-                    .add(RedisKeyUtil.RECOMMEND_KEY, String.valueOf(boardId), 0);
-                redisTemplate.opsForZSet()
-                    .add(RedisKeyUtil.POPULAR_KEY, String.valueOf(boardId), 0);
-            });
-        stopWatch.stop();
-        log.info("Execution Time for setting Redis Ranking : {}", stopWatch.prettyPrint());
+        rankingService.updatingNonRankedBoards();
     }
 
 }
