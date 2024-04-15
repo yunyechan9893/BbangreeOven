146370bd10fe25bed517044ea0310bb3317d9d83
shoeone96 <dlwnddnjs96@gmail.com>
2024-04-15 14:53:47 +0900

    서비스 로직에서 바뀐 좋아요 여부 넣는 부분 변경 반영, ranking 체킹 부분 반영

--- src/main/java/com/bbangle/bbangle/board/repository/BoardQueryDSLRepository.java
+++ src/main/java/com/bbangle/bbangle/board/repository/BoardQueryDSLRepository.java
@@ -1,7 +1,9 @@
 package com.bbangle.bbangle.board.repository;
 
+import com.bbangle.bbangle.board.domain.Board;
 import com.bbangle.bbangle.board.dto.BoardDetailResponseDto;
 import com.bbangle.bbangle.board.dto.BoardResponseDto;
+import com.bbangle.bbangle.common.sort.SortType;
 import com.bbangle.bbangle.page.BoardCustomPage;
 import com.bbangle.bbangle.board.dto.FilterRequest;
 import com.bbangle.bbangle.wishListFolder.domain.WishlistFolder;
@@ -12,9 +14,9 @@ import org.springframework.data.domain.Slice;
 
 public interface BoardQueryDSLRepository {
 
-    BoardCustomPage<List<BoardResponseDto>> getBoardResponseDto(
+    BoardCustomPage<List<BoardResponseDto>> getBoardResponseDtoWithoutLogin(
         FilterRequest filterRequest,
-        List<Long> matchedIdx,
+        SortType sort,
         Long cursorId
     );
 
@@ -30,6 +32,9 @@ public interface BoardQueryDSLRepository {
     HashMap<Long, String> getAllBoardTitle();
 
     List<BoardResponseDto> updateLikeStatus(List<Long> matchedIdx, List<BoardResponseDto> content);
+    List<Board> checkingNullRanking();
+
+    BoardCustomPage<List<BoardResponseDto>> getBoardResponseWithLogin(FilterRequest filterRequest, SortType sort, Long cursorId);
 
 }
 
