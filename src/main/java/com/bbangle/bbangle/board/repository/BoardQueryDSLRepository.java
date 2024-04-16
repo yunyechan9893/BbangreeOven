1acc0784882b0db14b4e1e77531961864b989531
shoeone96 <dlwnddnjs96@gmail.com>
2024-04-16 11:05:50 +0900

    24시간 이전 데이터 업데이트 로직 수정

--- src/main/java/com/bbangle/bbangle/board/repository/BoardQueryDSLRepository.java
+++ src/main/java/com/bbangle/bbangle/board/repository/BoardQueryDSLRepository.java
@@ -3,6 +3,7 @@ package com.bbangle.bbangle.board.repository;
 import com.bbangle.bbangle.board.domain.Board;
 import com.bbangle.bbangle.board.dto.BoardDetailResponseDto;
 import com.bbangle.bbangle.board.dto.BoardResponseDto;
+import com.bbangle.bbangle.board.dto.CursorInfo;
 import com.bbangle.bbangle.common.sort.SortType;
 import com.bbangle.bbangle.page.BoardCustomPage;
 import com.bbangle.bbangle.board.dto.FilterRequest;
@@ -17,7 +18,7 @@ public interface BoardQueryDSLRepository {
     BoardCustomPage<List<BoardResponseDto>> getBoardResponseDtoWithoutLogin(
         FilterRequest filterRequest,
         SortType sort,
-        Long cursorId
+        CursorInfo cursorInfo
     );
 
     Slice<BoardResponseDto> getAllByFolder(
@@ -30,11 +31,9 @@ public interface BoardQueryDSLRepository {
     BoardDetailResponseDto getBoardDetailResponse(Long memberId, Long boardId);
 
     HashMap<Long, String> getAllBoardTitle();
-
-    List<BoardResponseDto> updateLikeStatus(List<Long> matchedIdx, List<BoardResponseDto> content);
     List<Board> checkingNullRanking();
 
-    BoardCustomPage<List<BoardResponseDto>> getBoardResponseWithLogin(FilterRequest filterRequest, SortType sort, Long cursorId);
+    BoardCustomPage<List<BoardResponseDto>> getBoardResponseWithLogin(FilterRequest filterRequest, SortType sort, CursorInfo cursorId);
 
 }
 
