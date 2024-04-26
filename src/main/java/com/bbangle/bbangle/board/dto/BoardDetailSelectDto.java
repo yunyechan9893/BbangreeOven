ac4241934e5ed1b440d36f65e0650faca4640d5d
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-04-26 18:00:30 +0900

    8개행에서 4개행으로 변경

--- src/main/java/com/bbangle/bbangle/board/dto/BoardDetailSelectDto.java
+++ src/main/java/com/bbangle/bbangle/board/dto/BoardDetailSelectDto.java
@@ -6,16 +6,18 @@ import java.util.List;
 
 @Builder
 public record BoardDetailSelectDto(
-        Long boardId,
-        String thumbnail,
-        List<BoardImgDto> images,
-        String title,
-        int price,
-        BoardAvailableDayDto orderAvailableDays,
-        String purchaseUrl,
-        Boolean isWished,
-        Boolean isBundled,
-        List<BoardDetailDto> detail,
-        List<String> tags,
-        List<ProductDto> products
-) {}
+    Long boardId,
+    String thumbnail,
+    List<BoardImgDto> images,
+    String title,
+    int price,
+    BoardAvailableDayDto orderAvailableDays,
+    String purchaseUrl,
+    Boolean isWished,
+    Boolean isBundled,
+    List<BoardDetailDto> detail,
+    List<String> tags,
+    List<ProductDto> products
+) {
+
+}
