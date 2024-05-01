b7a8701ab6b69f909caf112adcf34bf6e4ff625a
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-01 16:03:24 +0900

    wishlist validator validate 조건 추가

--- src/main/java/com/bbangle/bbangle/wishlist/validator/WishListFolderValidator.java
+++ src/main/java/com/bbangle/bbangle/wishlist/validator/WishListFolderValidator.java
@@ -2,13 +2,20 @@ package com.bbangle.bbangle.wishlist.validator;
 
 import com.bbangle.bbangle.exception.BbangleException;
 import com.bbangle.bbangle.exception.BbangleErrorCode;
+import com.bbangle.bbangle.member.domain.Member;
 import java.util.Objects;
 
 public class WishListFolderValidator {
 
-    public static void validate(String title) {
-        if (Objects.isNull(title) || title.length() > 12) {
+    public static void validateTitle(String title) {
+        if (Objects.isNull(title) || title.isBlank() || title.length() > 12) {
             throw new BbangleException(BbangleErrorCode.INVALID_FOLDER_TITLE);
+        }
+    }
+
+    public static void validateMember(Member member) {
+        if(Objects.isNull(member)){
+            throw new BbangleException(BbangleErrorCode.INVALID_FOLDER_MEMBER);
         }
     }
 
