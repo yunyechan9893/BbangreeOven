0040636bb88c64d59c2dc71e82a1e74d25bd05f9
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-01 17:00:30 +0900

    FolderUpdateDto 생성 시 생성자 검증 로직 변경

--- src/main/java/com/bbangle/bbangle/wishlist/dto/FolderUpdateDto.java
+++ src/main/java/com/bbangle/bbangle/wishlist/dto/FolderUpdateDto.java
@@ -1,14 +1,15 @@
 package com.bbangle.bbangle.wishlist.dto;
 
+import com.bbangle.bbangle.wishlist.validator.WishListFolderValidator;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record FolderUpdateDto(
-    @NotBlank(message = INVALID_FOLDER_TITLE)
-    @Size(max = 12, message = INVALID_FOLDER_TITLE)
     String title
 ) {
 
-    private static final String INVALID_FOLDER_TITLE = "folder 제목을 확인해주십시오.";
+    public FolderUpdateDto {
+        WishListFolderValidator.validateTitle(title);
+    }
 
 }
