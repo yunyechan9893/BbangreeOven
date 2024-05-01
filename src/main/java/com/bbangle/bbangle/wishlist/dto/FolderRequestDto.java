a3b9edf6934283a20a074ec43d5a1ab54dc2777b
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-01 16:06:41 +0900

    메서드 명 변경

--- src/main/java/com/bbangle/bbangle/wishlist/dto/FolderRequestDto.java
+++ src/main/java/com/bbangle/bbangle/wishlist/dto/FolderRequestDto.java
@@ -1,15 +1,13 @@
 package com.bbangle.bbangle.wishlist.dto;
 
 import com.bbangle.bbangle.wishlist.validator.WishListFolderValidator;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record FolderRequestDto(
     String title
 ) {
 
     public FolderRequestDto {
-        WishListFolderValidator.validate(title);
+        WishListFolderValidator.validateTitle(title);
     }
 
 }
