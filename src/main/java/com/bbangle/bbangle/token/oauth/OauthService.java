97310d7e2dc34eac69776d6b54770ff374cad409
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-01 16:04:12 +0900

    멤버 생성 및 기본 폴더 부여하는 로직 login 관련 로직에서 분리

--- src/main/java/com/bbangle/bbangle/token/oauth/OauthService.java
+++ src/main/java/com/bbangle/bbangle/token/oauth/OauthService.java
@@ -3,6 +3,7 @@ package com.bbangle.bbangle.token.oauth;
 import com.bbangle.bbangle.common.redis.repository.RefreshTokenRepository;
 import com.bbangle.bbangle.member.domain.Member;
 import com.bbangle.bbangle.member.repository.MemberRepository;
+import com.bbangle.bbangle.member.service.MemberService;
 import com.bbangle.bbangle.token.domain.RefreshToken;
 import com.bbangle.bbangle.token.jwt.TokenProvider;
 import com.bbangle.bbangle.token.oauth.domain.OauthServerType;
@@ -22,41 +23,31 @@ public class OauthService {
 
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofHours(3);
-    private static final String DEFAULT_FOLDER_NAME = "기본 폴더";
+
     private final OauthMemberClientComposite oauthMemberClientComposite;
     private final MemberRepository memberRepository;
-    private final WishListFolderService folderService;
+    private final MemberService memberService;
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
     public LoginTokenResponse login(OauthServerType oauthServerType, String authCode) {
         Member oauthMember = oauthMemberClientComposite.fetch(oauthServerType, authCode);
-        String nickname = oauthMember.getNickname();
         //TODO 구글, 카카오 식별자 필요
-        //카카오
         Member saved = memberRepository.findByProviderAndProviderId(oauthMember.getProvider(),
                 oauthMember.getProviderId())
-            .orElseGet(() -> {
-                Member newMember = Member.builder()
-                    .nickname(nickname)
-                    .provider(oauthMember.getProvider())
-                    .providerId(oauthMember.getProviderId())
-                    .profile(oauthMember.getProfile())
-                    .build();
-                memberRepository.save(newMember);
-                Long newMemberId = newMember.getId();
-                //기본 위시리스트 폴더 추가
-                folderService.create(newMemberId, new FolderRequestDto(DEFAULT_FOLDER_NAME));
-                return newMember;
-            });
+            .orElseGet(() -> memberService.getFirstJoinedMember(oauthMember));
+
         String refreshToken = tokenProvider.generateToken(saved, REFRESH_TOKEN_DURATION);
         String accessToken = tokenProvider.generateToken(saved, ACCESS_TOKEN_DURATION);
+
         Optional<RefreshToken> refreshTokenByMemberId =
             refreshTokenRepository.findByMemberId(saved.getId());
+
         refreshTokenByMemberId.ifPresentOrElse(
             token -> refreshTokenByMemberId.get().update(refreshToken),
             () -> saveRefreshToken(refreshToken, saved));
+
         return new LoginTokenResponse(accessToken, refreshToken);
     }
 
