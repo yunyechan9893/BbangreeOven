782147936cfe63738fe8bc16ff37caad27ee1cd5
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-01 16:06:28 +0900

    폴더 이름과 멤버로 이미 해당 유저에게 존재하는지 여부를 찾는 메서드 추가

--- src/main/java/com/bbangle/bbangle/wishlist/repository/WishListFolderRepository.java
+++ src/main/java/com/bbangle/bbangle/wishlist/repository/WishListFolderRepository.java
@@ -10,13 +10,10 @@ import org.springframework.data.repository.query.Param;
 public interface WishListFolderRepository extends JpaRepository<WishlistFolder, Long>,
     WishListFolderQueryDSLRepository {
 
-    @Query(
-        value = "select count(folder) from WishlistFolder folder where folder.member = :member and folder.isDeleted = false "
-    )
-    int getFolderCount(
-        @Param("member")
-        Member member
-    );
+    @Query(value = "select count(folder) from WishlistFolder folder where folder.member = :member and folder.isDeleted = false ")
+    int getFolderCount(@Param("member") Member member);
+
+    boolean existsByFolderNameAndMember(String folderName, Member member);
 
     Optional<WishlistFolder> findByMemberAndId(Member member, Long folderId);
 
