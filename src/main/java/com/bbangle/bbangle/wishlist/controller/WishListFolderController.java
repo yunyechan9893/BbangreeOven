a36303e1890553974cd9cca3ce27846a8688d1ae
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-01 14:53:41 +0900

    @AuthenticationPrincipal 반영, @Valid 어노테이션 삭제(생성자 검증 도입하여 어노테이션 사용하지 않음)

--- src/main/java/com/bbangle/bbangle/wishlist/controller/WishListFolderController.java
+++ src/main/java/com/bbangle/bbangle/wishlist/controller/WishListFolderController.java
@@ -8,6 +8,7 @@ import com.bbangle.bbangle.util.SecurityUtils;
 import com.bbangle.bbangle.wishlist.service.WishListFolderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,9 +29,10 @@ public class WishListFolderController {
     @PostMapping
     public CommonResult make(
         @RequestBody
-        @Valid FolderRequestDto requestDto
+        FolderRequestDto requestDto,
+        @AuthenticationPrincipal
+        Long memberId
     ) {
-        Long memberId = SecurityUtils.getMemberId();
         folderService.create(memberId, requestDto);
         return responseService.getSuccessResult();
     }
