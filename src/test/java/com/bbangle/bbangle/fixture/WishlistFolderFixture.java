dfde9fe3532b8936bf530f5f83edd79c58b421cf
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-03 15:19:31 +0900

    wishlistfolder 생성 Fixture 생성

---
+++ src/test/java/com/bbangle/bbangle/fixture/WishlistFolderFixture.java
@@ -0,0 +1,26 @@
+package com.bbangle.bbangle.fixture;
+
+import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.wishlist.domain.WishlistFolder;
+import net.datafaker.Faker;
+
+public class WishlistFolderFixture {
+
+    private static final Faker faker = new Faker();
+
+    public static WishlistFolder createWishlistFolder(Member member){
+        String title = faker.book()
+            .title();
+
+        if(title.length() > 12){
+            title = title.substring(0, 12);
+        }
+
+        return WishlistFolder.builder()
+            .folderName(title)
+            .member(member)
+            .isDeleted(false)
+            .build();
+    }
+
+}
