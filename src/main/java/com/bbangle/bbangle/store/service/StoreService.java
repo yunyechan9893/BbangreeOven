ba60f63355ab01fe0232dc25357303eaab33c88f
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-05 16:51:07 +0900

    StoreService Interface -> StoreService 클래스로 변경

--- src/main/java/com/bbangle/bbangle/store/service/StoreService.java
+++ src/main/java/com/bbangle/bbangle/store/service/StoreService.java
@@ -1,18 +1,54 @@
 package com.bbangle.bbangle.store.service;
 
+import com.bbangle.bbangle.exception.BbangleException;
+import com.bbangle.bbangle.member.domain.Member;
+import com.bbangle.bbangle.member.repository.MemberRepository;
+
 import com.bbangle.bbangle.board.dto.StoreAllBoardDto;
+
 import com.bbangle.bbangle.page.StoreCustomPage;
 import com.bbangle.bbangle.store.dto.StoreDetailResponseDto;
 import com.bbangle.bbangle.store.dto.StoreResponseDto;
+import com.bbangle.bbangle.store.repository.StoreRepository;
+import com.bbangle.bbangle.util.SecurityUtils;
 import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Service;
 
-public interface StoreService {
+@Service
+@RequiredArgsConstructor
+public class StoreService {
 
-    StoreDetailResponseDto getStoreDetailResponse(Long memberId, Long StoreId);
+    private final StoreRepository storeRepository;
+    private final MemberRepository memberRepository;
 
-    SliceImpl<StoreAllBoardDto> getAllBoard(int page,Long memberId, Long StoreId);
+    public StoreDetailResponseDto getStoreDetailResponse(Long memberId, Long storeId) {
+        return memberId > 1L ?
+            storeRepository.getStoreDetailResponseDtoWithLike(memberId, storeId) :
+            storeRepository.getStoreDetailResponseDto(storeId);
 
-    StoreCustomPage<List<StoreResponseDto>> getList(Long cursorId);
+    }
+
+    public SliceImpl<StoreAllBoardDto> getAllBoard(int page, Long memberId, Long storeId) {
+        int PAGE_SIZE = 10;
+
+        return memberId > 1L ?
+            storeRepository.getAllBoardWithLike(PageRequest.of(page, PAGE_SIZE), memberId, storeId) :
+                storeRepository.getAllBoard(PageRequest.of(page, PAGE_SIZE), storeId);
+    }
+
+    public StoreCustomPage<List<StoreResponseDto>> getList(Long cursorId, Long memberId) {
+        if(Objects.isNull(memberId)){
+            return storeRepository.findNextCursorPageWithoutLogin(cursorId);
+        }
+
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(BbangleException::new);
+
+        return storeRepository.findNextCursorPageWithLogin(cursorId, member);
+    }
 
 }
