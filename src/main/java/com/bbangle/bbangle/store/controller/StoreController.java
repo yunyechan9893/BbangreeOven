2ae515009a1841fef0786fa40a11b70b27908121
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-05-10 23:39:44 +0900

    * 임포트 제거

--- src/main/java/com/bbangle/bbangle/store/controller/StoreController.java
+++ src/main/java/com/bbangle/bbangle/store/controller/StoreController.java
@@ -10,7 +10,11 @@ import com.bbangle.bbangle.store.service.StoreService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
