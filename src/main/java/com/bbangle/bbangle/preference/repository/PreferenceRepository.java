8bc92c325703ccf8ce08eb871d983a988790857b
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-11 16:21:47 +0900

    멤버의 preference 가져오는 repository 쿼리문 작성

--- src/main/java/com/bbangle/bbangle/preference/repository/PreferenceRepository.java
+++ src/main/java/com/bbangle/bbangle/preference/repository/PreferenceRepository.java
@@ -1,12 +1,16 @@
 package com.bbangle.bbangle.preference.repository;
 
+import com.bbangle.bbangle.preference.domain.MemberPreference;
 import com.bbangle.bbangle.preference.domain.Preference;
 import com.bbangle.bbangle.preference.domain.PreferenceType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PreferenceRepository extends JpaRepository<Preference, Long>, PreferenceQueryDSLRepository {
 
     Optional<Preference> findByPreferenceType(PreferenceType preferenceType);
 
+    @Query("select p from Preference p join MemberPreference mp on mp.preference = p where mp = :memberPreference")
+    Optional<Preference> findPreferenceTypeWithMemberPreference(MemberPreference memberPreference);
 }
