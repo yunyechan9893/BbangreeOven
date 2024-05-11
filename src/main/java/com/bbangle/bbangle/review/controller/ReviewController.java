a8946251a7ca6cb953da8cef981c5acc764a85f2
sikdong <zmstnv1119@naver.com>
2024-05-11 22:38:11 +0900

    refactor : 리뷰 요청 dto validation 추가

--- src/main/java/com/bbangle/bbangle/review/controller/ReviewController.java
+++ src/main/java/com/bbangle/bbangle/review/controller/ReviewController.java
@@ -4,17 +4,14 @@ import com.bbangle.bbangle.common.dto.CommonResult;
 import com.bbangle.bbangle.common.service.ResponseService;
 import com.bbangle.bbangle.review.dto.ReviewRequest;
 import com.bbangle.bbangle.review.service.ReviewService;
-import com.bbangle.bbangle.util.SecurityUtils;
-import java.util.List;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,8 +23,7 @@ public class ReviewController {
 
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public CommonResult makeReview(
-        @ModelAttribute ReviewRequest reviewRequest,
-        //@RequestPart(required = false) List<MultipartFile> photos,
+        @Valid @ModelAttribute ReviewRequest reviewRequest,
         @AuthenticationPrincipal Long memberId
     ){
         reviewService.makeReview(reviewRequest, memberId);
--- src/main/java/com/bbangle/bbangle/review/dto/ReviewRequest.java
+++ src/main/java/com/bbangle/bbangle/review/dto/ReviewRequest.java
@@ -1,16 +1,24 @@
 package com.bbangle.bbangle.review.dto;
 
 import com.bbangle.bbangle.common.domain.Badge;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.List;
-import lombok.Builder;
-import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 public record ReviewRequest(
+    @NotNull
+    @Size(min = 3, max = 3)
     List<Badge> badges,
+    @NotNull
+    @DecimalMin(value = "0")
+    @DecimalMax(value = "5")
     BigDecimal rate,
     String content,
+    @NotNull
     Long boardId,
     List<MultipartFile> photos
 ) {
