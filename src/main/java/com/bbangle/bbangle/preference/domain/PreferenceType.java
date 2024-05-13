b90ce4f37b24e99d2a6709ee36de639907038456
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-13 18:36:33 +0900

    enum에 PreferenceType description  추가

--- src/main/java/com/bbangle/bbangle/preference/domain/PreferenceType.java
+++ src/main/java/com/bbangle/bbangle/preference/domain/PreferenceType.java
@@ -1,14 +1,24 @@
 package com.bbangle.bbangle.preference.domain;
 
 public enum PreferenceType {
-    DIET,
-    MUSCLE_GROW,
-    CONSTITUTION,
-    VEGAN,
-    DIET_MUSCLE_GROW,
-    DIET_CONSTITUTION,
-    DIET_VEGAN,
-    MUSCLE_GROW_CONSTITUTION,
-    MUSCLE_GROW_VEGAN,
-    CONSTITUTION_VEGAN;
+    DIET("다이어트"),
+    MUSCLE_GROW("근육 증가"),
+    CONSTITUTION("체질 개선"),
+    VEGAN("비건"),
+    DIET_MUSCLE_GROW("다이어트 + 근육 증가"),
+    DIET_CONSTITUTION("다이어트 + 체질 개선"),
+    DIET_VEGAN("다이어트 + 비건"),
+    MUSCLE_GROW_CONSTITUTION("근육 증가 + 체질 개선"),
+    MUSCLE_GROW_VEGAN("근육 증가 + 비건"),
+    CONSTITUTION_VEGAN("체질 개선 + 비건");
+
+    private final String description;
+
+    PreferenceType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
