7eaf63beb0440620bdb5a7909c775cb9b3725810
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-13 18:30:38 +0900

    사용하지 않는 메서드 삭제

--- src/main/java/com/bbangle/bbangle/preference/repository/PreferenceRepositoryImpl.java
+++ src/main/java/com/bbangle/bbangle/preference/repository/PreferenceRepositoryImpl.java
@@ -1,11 +1,4 @@
 package com.bbangle.bbangle.preference.repository;
-
-import com.bbangle.bbangle.member.domain.Member;
-import com.bbangle.bbangle.preference.domain.QMemberPreference;
-import com.bbangle.bbangle.preference.domain.QPreference;
-import com.bbangle.bbangle.preference.dto.MemberPreferenceResponse;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -13,20 +6,5 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class PreferenceRepositoryImpl implements PreferenceQueryDSLRepository{
 
-    private final JPAQueryFactory queryFactory;
-    private static final QPreference preference = QPreference.preference;
-    private static final QMemberPreference memberPreference = QMemberPreference.memberPreference;
-
-
-    @Override
-    public MemberPreferenceResponse getMemberPreference(Member member) {
-        return queryFactory.select(Projections.constructor(
-            MemberPreferenceResponse.class,
-            preference.preferenceType
-        )).from(preference)
-            .join(preference, memberPreference.preference)
-            .where(memberPreference.member.eq(member))
-            .fetchOne();
-    }
 
 }
