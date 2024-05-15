dbc78988280cb0e0cdf92a28e5afdff0c8a16c13
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-05-16 01:02:57 +0900

    스네이크 케이스 -> 카멜케이스로 변경

--- src/main/java/com/bbangle/bbangle/board/dto/ProductDto.java
+++ src/main/java/com/bbangle/bbangle/board/dto/ProductDto.java
@@ -10,25 +10,15 @@ public record ProductDto(
     Long productId,
     String productTitle,
     Category category,
-    Boolean gluten_free_tag,
-    Boolean high_protein_tag,
-    Boolean sugar_free_tag,
-    Boolean vegan_tag,
-    Boolean ketogenic_tag
+    Boolean glutenFreeTag,
+    Boolean highProteinTag,
+    Boolean sugarFreeTag,
+    Boolean veganTag,
+    Boolean ketogenicTag
 
 ) {
     @QueryProjection
     public ProductDto {
 
-    }
-
-    public TagDto toTagDto(){
-        return TagDto.builder()
-            .gluten_free_tag(gluten_free_tag)
-            .high_protein_tag(high_protein_tag)
-            .sugar_free_tag(sugar_free_tag)
-            .vegan_tag(vegan_tag)
-            .ketogenic_tag(ketogenic_tag)
-            .build();
     }
 }
