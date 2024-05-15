c89c8d1735d93b92f0e370d4348aec0b28978f2e
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-15 15:16:17 +0900

    sort type 인기순, 추천순만 남기고 분리

--- src/main/java/com/bbangle/bbangle/common/sort/SortType.java
+++ src/main/java/com/bbangle/bbangle/common/sort/SortType.java
@@ -1,26 +1,32 @@
 package com.bbangle.bbangle.common.sort;
 
 import com.bbangle.bbangle.board.domain.QBoard;
+import com.bbangle.bbangle.board.repository.folder.cursor.CursorGenerator;
+import com.bbangle.bbangle.board.repository.folder.cursor.LowPriceCursorGenerator;
+import com.bbangle.bbangle.board.repository.folder.cursor.PopularCursorGenerator;
+import com.bbangle.bbangle.board.repository.folder.cursor.WishListRecentCursorGenerator;
 import com.bbangle.bbangle.ranking.domain.QRanking;
 import com.bbangle.bbangle.wishlist.domain.QWishListBoard;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import lombok.Getter;
 
 @Getter
 public enum SortType {
     RECENT(QBoard.board.createdAt::desc),
-    LOW_PRICE(QBoard.board.price::asc),
-    POPULAR(QRanking.ranking.popularScore::desc),
-    WISHLIST_RECENT(QWishListBoard.wishListBoard.id::desc),
-    RECOMMEND(QRanking.ranking.recommendScore::desc);
+    POPULAR(QRanking.ranking.popularScore::desc);
 
-    SortType(Supplier<OrderSpecifier<?>> setOrder) {
+    private final Supplier<OrderSpecifier<?>> setOrder;
+
+    SortType(Supplier<OrderSpecifier<?>> setOrder
+    ) {
         this.setOrder = setOrder;
     }
-    private final Supplier<OrderSpecifier<?>> setOrder;
 
     public OrderSpecifier<?> getOrderSpecifier(){
         return setOrder.get();
     }
+
 }
