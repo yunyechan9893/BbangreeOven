2c5ff68d6578b1768fb3982c1296240e21b2c13e
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-22 14:17:29 +0900

    BoardResponseDao를 가공하는 메서드 추가

--- src/main/java/com/bbangle/bbangle/board/repository/folder/BoardPageGenerator.java
+++ src/main/java/com/bbangle/bbangle/board/repository/folder/BoardPageGenerator.java
@@ -2,16 +2,18 @@ package com.bbangle.bbangle.board.repository.folder;
 
 import static com.bbangle.bbangle.board.repository.BoardRepositoryImpl.BOARD_PAGE_SIZE;
 
-import com.bbangle.bbangle.board.domain.Board;
-import com.bbangle.bbangle.board.domain.Product;
+import com.bbangle.bbangle.board.dao.BoardResponseDao;
+import com.bbangle.bbangle.board.dao.TagsDao;
 import com.bbangle.bbangle.board.domain.TagEnum;
 import com.bbangle.bbangle.board.dto.BoardResponseDto;
 import com.bbangle.bbangle.page.BoardCustomPage;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -26,47 +28,105 @@ public class BoardPageGenerator {
     private static final Long NO_NEXT_CURSOR = -1L;
     private static final Long HAS_NEXT_PAGE_SIZE = BOARD_PAGE_SIZE + 1L;
 
-    public BoardCustomPage<List<BoardResponseDto>> getBoardPage(List<Board> allByFolder){
-        if(allByFolder.isEmpty()){
+    public BoardCustomPage<List<BoardResponseDto>> getBoardPage(List<BoardResponseDao> allByFolder) {
+        if (allByFolder.isEmpty()) {
             return BoardCustomPage.emptyPage();
         }
 
         List<BoardResponseDto> boardResponseDtos = convertToBoardResponse(allByFolder);
+
         Long nextCursor = NO_NEXT_CURSOR;
         boolean hasNext = false;
-        if(allByFolder.size() == HAS_NEXT_PAGE_SIZE){
+        if (boardResponseDtos.size() == HAS_NEXT_PAGE_SIZE) {
             hasNext = true;
-            nextCursor = allByFolder.get(allByFolder.size()-1).getId();
+            nextCursor = allByFolder.get(boardResponseDtos.size() - 1).boardId();
         }
+        boardResponseDtos = boardResponseDtos.stream().limit(BOARD_PAGE_SIZE).toList();
+
         return BoardCustomPage.from(boardResponseDtos,
             nextCursor, hasNext);
     }
 
-    private List<BoardResponseDto> convertToBoardResponse(List<Board> boards) {
-        Map<Long, List<String>> tagMapByBoardId = boards.stream()
-            .collect(Collectors.toMap(
-                Board::getId,
-                board -> extractTags(board.getProductList())
-            ));
+    private List<BoardResponseDto> convertToBoardResponse(List<BoardResponseDao> boardResponseDaoList) {
+        Map<Long, List<String>> tagMapByBoardId = getTagListFromBoardResponseDao(boardResponseDaoList);
 
-        return boards.stream()
-            .limit(BOARD_PAGE_SIZE)
-            .map(board -> BoardResponseDto.from(board, tagMapByBoardId.get(board.getId())))
+        Map<Long, Boolean> isBundled = getIsBundled(boardResponseDaoList);
+
+        boardResponseDaoList = removeDuplicatesByBoardId(boardResponseDaoList);
+
+        return boardResponseDaoList.stream()
+            .map(boardDao -> BoardResponseDto.inFolder(boardDao, isBundled.get(boardDao.boardId()),
+                tagMapByBoardId.get(boardDao.boardId())))
             .toList();
     }
 
-    private List<String> extractTags(List<Product> products) {
-        if (Objects.isNull(products) || products.isEmpty()) {
+    private Map<Long, List<String>> getTagListFromBoardResponseDao(List<BoardResponseDao> boardResponseDaoList) {
+        Map<Long, List<String>> tagMapByBoardId = boardResponseDaoList.stream()
+            .collect(Collectors.toMap(
+                BoardResponseDao::boardId,
+                board -> new ArrayList<>(Collections.singletonList(board.tagsDao())),
+                (existingList, newList) -> {
+                    existingList.addAll(newList);
+                    return existingList;
+                }))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                Entry::getKey,
+                entry -> extractTags(entry.getValue())
+                    .stream()
+                    .toList()
+            ));
+        return tagMapByBoardId;
+    }
+
+    private static Map<Long, Boolean> getIsBundled(List<BoardResponseDao> boardResponseDaoList) {
+        Map<Long, Boolean> isBundled = boardResponseDaoList
+            .stream()
+            .collect(Collectors.toMap(
+                BoardResponseDao::boardId,
+                board -> new HashSet<>(Collections.singleton(board.category())),
+                (existingSet, newSet) -> {
+                    existingSet.addAll(newSet);
+                    return existingSet;
+                }
+            ))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                Entry::getKey,
+                entry -> entry.getValue()
+                    .size() > 1)
+            );
+        return isBundled;
+    }
+
+    private List<BoardResponseDao> removeDuplicatesByBoardId(List<BoardResponseDao> boardResponseDaos) {
+        Map<Long, BoardResponseDao> uniqueBoardMap = boardResponseDaos.stream()
+            .collect(Collectors.toMap(
+                BoardResponseDao::boardId,
+                boardResponseDao -> boardResponseDao,
+                (existing, replacement) -> existing,
+                LinkedHashMap::new
+            ));
+
+        return uniqueBoardMap.values()
+            .stream()
+            .toList();
+    }
+
+    private List<String> extractTags(List<TagsDao> tagsDaoList) {
+        if (Objects.isNull(tagsDaoList) || tagsDaoList.isEmpty()) {
             return Collections.emptyList();
         }
 
         HashSet<String> tags = new HashSet<>();
-        for (Product dto : products) {
-            addTagIfTrue(tags, dto.isGlutenFreeTag(), TagEnum.GLUTEN_FREE.label());
-            addTagIfTrue(tags, dto.isHighProteinTag(), TagEnum.HIGH_PROTEIN.label());
-            addTagIfTrue(tags, dto.isSugarFreeTag(), TagEnum.SUGAR_FREE.label());
-            addTagIfTrue(tags, dto.isVeganTag(), TagEnum.VEGAN.label());
-            addTagIfTrue(tags, dto.isKetogenicTag(), TagEnum.KETOGENIC.label());
+        for (TagsDao dto : tagsDaoList) {
+            addTagIfTrue(tags, dto.glutenFreeTag(), TagEnum.GLUTEN_FREE.label());
+            addTagIfTrue(tags, dto.highProteinTag(), TagEnum.HIGH_PROTEIN.label());
+            addTagIfTrue(tags, dto.sugarFreeTag(), TagEnum.SUGAR_FREE.label());
+            addTagIfTrue(tags, dto.veganTag(), TagEnum.VEGAN.label());
+            addTagIfTrue(tags, dto.ketogenicTag(), TagEnum.KETOGENIC.label());
         }
         return new ArrayList<>(tags);
     }
