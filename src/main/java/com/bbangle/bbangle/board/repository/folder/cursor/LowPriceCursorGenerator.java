b3490f30dbcf7c18cf7c2c5b56cba9b73e4db227
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-22 14:15:50 +0900

    cursorId에 해당하는 board 정보가 없을 경우의 예외처리 추가

--- src/main/java/com/bbangle/bbangle/board/repository/folder/cursor/LowPriceCursorGenerator.java
+++ src/main/java/com/bbangle/bbangle/board/repository/folder/cursor/LowPriceCursorGenerator.java
@@ -1,8 +1,11 @@
 package com.bbangle.bbangle.board.repository.folder.cursor;
 
 import com.bbangle.bbangle.board.domain.QBoard;
+import com.bbangle.bbangle.exception.BbangleErrorCode;
+import com.bbangle.bbangle.exception.BbangleException;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +23,13 @@ public class LowPriceCursorGenerator implements CursorGenerator{
         if(cursorId == null){
             return cursorBuilder;
         }
+
+        Optional.ofNullable(queryFactory.select(board.id)
+            .from(board)
+            .where(board.id.eq(cursorId))
+            .fetchOne())
+            .orElseThrow(() -> new BbangleException(BbangleErrorCode.BOARD_NOT_FOUND));
+
         Integer price = queryFactory
             .select(board.price)
             .from(board)
