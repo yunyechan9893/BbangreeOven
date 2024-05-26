ab2fd1a73bbebacf22acfe163143571069859c64
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-05-26 18:37:41 +0900

    of 정적 메서드 구현

--- src/main/java/com/bbangle/bbangle/board/dto/ProductResponse.java
+++ src/main/java/com/bbangle/bbangle/board/dto/ProductResponse.java
@@ -9,4 +9,13 @@ public record ProductResponse(
     List<BoardDetailProductDto> products
 ) {
 
+    public static ProductResponse of(
+        Boolean boardIsBundled,
+        List<BoardDetailProductDto> products
+    ) {
+        return ProductResponse.builder()
+            .boardIsBundled(boardIsBundled)
+            .products(products)
+            .build();
+    }
 }
